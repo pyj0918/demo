22d5d6a614b5cfc7176906ace08ec8713b7b5760
pyj0918 <317862554@qq.com>
2016-09-24 18:23:22 +0800

    mybatis01

--- mybatis/mybatis-01/src/com/test2/App2.java
+++ mybatis/mybatis-01/src/com/test2/App2.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import com.test1.User;
-import com.test3.UserMapper;
 import com.util.MyBatisUtil;
 
 public class App2 {
@@ -62,19 +61,5 @@ public class App2 {
 		List<User> users = session.selectList("com.test2.userMapper.getAll");
 		session.close();
 		System.out.println(users);
-	}
-
-	// 使用注解的方式，进行添加数据
-	@Test
-	public void testAnnotation() {
-		SqlSession session = MyBatisUtil.getFactory().openSession();
-		UserMapper mapper = session.getMapper(UserMapper.class);
-		User entity = new User();
-		entity.setAge(36);
-		entity.setName("hello333");
-		int row = mapper.add(entity);
-		session.commit();
-		session.close();
-		System.out.println(row);
 	}
 }
